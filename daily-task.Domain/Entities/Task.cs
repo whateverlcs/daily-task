3c8bf33d47da9eeb21cf9b6ae578a45d8fd3fcc5
lucasbatistao <lucassbatistao22@gmail.com>
2026-02-19 14:11:13 -0300

    feat(domain): [DT-1] add task entity and domain enums

---
+++ daily-task.Domain/Entities/Task.cs
@@ -0,0 +1,17 @@
+﻿using daily_task.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daily_task.Domain.Entities
+{
+    public class Task : EntityBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public Priority Priority { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public Status Status { get; set; }
+    }
+}
