af141e51bb0e97ddb63b10f90e3004965c858c30
lucasbatistao <lucassbatistao22@gmail.com>
2026-02-26 16:39:29 -0300

    feat: [DT-4] add useCase to get the task by id

---
+++ daily-task.Application/UseCases/Task/GetById/GetTaskByIdUseCase.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using daily_task.Application.Models;
+using daily_task.Domain.Repositories.Task;
+using daily_task.Exceptions;
+using daily_task.Exceptions.ExceptionsBase;
+
+namespace daily_task.Application.UseCases.Task.GetById
+{
+    public class GetTaskByIdUseCase : IGetTaskByIdUseCase
+    {
+        private readonly IMapper _mapper;
+        private readonly ITaskReadOnlyRepository _repository;
+
+        public GetTaskByIdUseCase(
+            IMapper mapper,
+            ITaskReadOnlyRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+
+        public async Task<TaskDisplayModel> Execute(long taskId)
+        {
+            var task = await _repository.GetById(taskId);
+
+            if (task is null)
+                throw new NotFoundException(ResourceMessagesException.TASK_NOT_FOUND);
+
+            var response = _mapper.Map<TaskDisplayModel>(task);
+
+            return response;
+        }
+    }
+}
