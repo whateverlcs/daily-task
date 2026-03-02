8b9f6a9d9e94ef4091d43954c964a7d73890159d
lucasbatistao <lucassbatistao22@gmail.com>
2026-03-02 15:42:52 -0300

    feat: [DT-16] add useCase listing rewards

--- daily-task.Application/DependencyInjectionExtension.cs
+++ daily-task.Application/DependencyInjectionExtension.cs
@@ -3,6 +3,7 @@ using daily_task.Application.UseCases.Profile.GetProfile;
 using daily_task.Application.UseCases.Profile.Register;
 using daily_task.Application.UseCases.Profile.Update;
 using daily_task.Application.UseCases.Rank.GetAllRanks;
+using daily_task.Application.UseCases.Reward.GetAllRewards;
 using daily_task.Application.UseCases.Task.GetAllTasks;
 using daily_task.Application.UseCases.Task.GetById;
 using daily_task.Application.UseCases.Task.Register;
@@ -43,6 +44,7 @@ namespace daily_task.Application
             services.AddScoped<IUpdateProfileUseCase, UpdateProfileUseCase>();
             services.AddScoped<IGetAllRanksUseCase, GetAllRanksUseCase>();
             services.AddScoped<IGetProfileUseCase, GetProfileUseCase>();
+            services.AddScoped<IGetAllRewardsUseCase, GetAllRewardsUseCase>();
         }
     }
 }
---
+++ daily-task.Application/Models/RewardDisplayModel.cs
@@ -0,0 +1,10 @@
+﻿namespace daily_task.Application.Models
+{
+    public class RewardDisplayModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Gold { get; set; } = string.Empty;
+        public bool Active { get; set; }
+    }
+}
