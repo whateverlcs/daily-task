1e0e72f3d3c7abe8d429aba5c541f46080b68a14
lucasbatistao <lucassbatistao22@gmail.com>
2026-03-02 16:10:26 -0300

    feat: [DT-18] useCases for edit reward

---
+++ daily-task.Application/UseCases/Reward/GetById/IGetRewardByIdUseCase.cs
@@ -0,0 +1,9 @@
+﻿using daily_task.Application.Models;
+
+namespace daily_task.Application.UseCases.Reward.GetById
+{
+    public interface IGetRewardByIdUseCase
+    {
+        System.Threading.Tasks.Task<RewardDisplayModel> Execute(long rewardId);
+    }
+}
---
+++ daily-task.Application/UseCases/Reward/Update/IUpdateRewardUseCase.cs
@@ -0,0 +1,9 @@
+﻿using daily_task.Application.Models;
+
+namespace daily_task.Application.UseCases.Reward.Update
+{
+    public interface IUpdateRewardUseCase
+    {
+        System.Threading.Tasks.Task<bool> Execute(long rewardId, RewardDisplayModel request);
+    }
+}
