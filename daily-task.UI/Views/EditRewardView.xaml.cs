dd2a29caebb715e262045e5113e0fa5f34a06c0d
lucasbatistao <lucassbatistao22@gmail.com>
2026-03-02 16:12:48 -0300

    feat: [DT-18] add view for edit reward

---
+++ daily-task.UI/Views/EditRewardView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace daily_task.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for EditRewardView.xaml
+    /// </summary>
+    public partial class EditRewardView : UserControl
+    {
+        public EditRewardView()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
