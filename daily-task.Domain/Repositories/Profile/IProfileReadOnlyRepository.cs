5395787d1c8a8e3f1fee7855942bc9a956e0cfa4
lucasbatistao <lucassbatistao22@gmail.com>
2026-03-02 08:57:24 -0300

    feat: [DT-6] create repositories for rank and profile

---
+++ daily-task.Domain/Repositories/Profile/IProfileReadOnlyRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace daily_task.Domain.Repositories.Profile
+{
+    public interface IProfileReadOnlyRepository
+    {
+        Task<Entities.Profile?> GetProfile();
+    }
+}
---
+++ daily-task.Infrastructure/DataAccess/Repositories/ProfileRepository.cs
@@ -0,0 +1,32 @@
+﻿using daily_task.Domain.Repositories.Profile;
+using Microsoft.EntityFrameworkCore;
+using Entities = daily_task.Domain.Entities;
+
+namespace daily_task.Infrastructure.DataAccess.Repositories
+{
+    public class ProfileRepository : IProfileWriteOnlyRepository, IProfileReadOnlyRepository, IProfileUpdateOnlyRepository
+    {
+        private readonly DailyTaskDbContext _dbContext;
+
+        public ProfileRepository(DailyTaskDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task Add(Entities.Profile profile) => await _dbContext.Profile.AddAsync(profile);
+
+        async Task<Entities.Profile?> IProfileReadOnlyRepository.GetProfile()
+        {
+            return await _dbContext
+                .Profile
+                .AsNoTracking()
+                .FirstOrDefaultAsync(profile => profile.Active);
+        }
+
+        async Task<Entities.Profile?> IProfileUpdateOnlyRepository.GetById(long profileId)
+        {
+            return await _dbContext
+                .Profile
+                .FirstOrDefaultAsync(profile => profile.Active && profile.Id == profileId);
+        }
+
+        public void Update(Entities.Profile profile) => _dbContext.Profile.Update(profile);
+    }
+}
