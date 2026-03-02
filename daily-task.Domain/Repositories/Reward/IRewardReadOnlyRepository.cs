f75300bfb5cb5c6a2c131e600ac0015704daf8b4
lucasbatistao <lucassbatistao22@gmail.com>
2026-03-02 15:36:29 -0300

    feat: [DT-16] add reward repository

---
+++ daily-task.Domain/Repositories/Reward/IRewardReadOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace daily_task.Domain.Repositories.Reward
+{
+    public interface IRewardReadOnlyRepository
+    {
+        Task<Entities.Reward?> GetById(long rewardId);
+
+        Task<IList<Entities.Reward>> GetAllRewardsActive();
+    }
+}
---
+++ daily-task.Infrastructure/DataAccess/Repositories/RewardRepository.cs
@@ -0,0 +1,41 @@
+﻿using daily_task.Domain.Repositories.Reward;
+using Microsoft.EntityFrameworkCore;
+using Entities = daily_task.Domain.Entities;
+
+namespace daily_task.Infrastructure.DataAccess.Repositories
+{
+    public class RewardRepository : IRewardWriteOnlyRepository, IRewardReadOnlyRepository, IRewardUpdateOnlyRepository
+    {
+        private readonly DailyTaskDbContext _dbContext;
+
+        public RewardRepository(DailyTaskDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task Add(Entities.Reward reward) => await _dbContext.Rewards.AddAsync(reward);
+
+        async Task<Entities.Reward?> IRewardReadOnlyRepository.GetById(long rewardId)
+        {
+            return await _dbContext
+                .Rewards
+                .AsNoTracking()
+                .FirstOrDefaultAsync(reward => reward.Active && reward.Id == rewardId);
+        }
+
+        async Task<IList<Entities.Reward>> IRewardReadOnlyRepository.GetAllRewardsActive()
+        {
+            return await _dbContext
+                .Rewards
+                .AsNoTracking()
+                .Where(reward => reward.Active)
+                .ToListAsync();
+        }
+
+        async Task<Entities.Reward?> IRewardUpdateOnlyRepository.GetById(long rewardId)
+        {
+            return await _dbContext
+                .Rewards
+                .FirstOrDefaultAsync(reward => reward.Active && reward.Id == rewardId);
+        }
+
+        public void Update(Entities.Reward reward) => _dbContext.Rewards.Update(reward);
+    }
+}
