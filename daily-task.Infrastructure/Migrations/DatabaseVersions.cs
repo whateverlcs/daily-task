b32ec00224d767f54f1f6d38ce9f756a19f1ac26
lucasbatistao <lucassbatistao22@gmail.com>
2026-03-03 10:48:20 -0300

    feat: [DT-8] add migrations to add the field username in table profile

--- daily-task.Infrastructure/Migrations/DatabaseVersions.cs
+++ daily-task.Infrastructure/Migrations/DatabaseVersions.cs
@@ -13,5 +13,6 @@ namespace daily_task.Infrastructure.Migrations
         public const int TABLE_RANK = 3;
         public const int TABLE_PROFILE = 4;
         public const int TABLE_REWARD = 5;
+        public const int ADD_USER_TABLE_PROFILE = 6;
     }
 }
